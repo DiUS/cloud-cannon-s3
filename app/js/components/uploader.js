d71619aa942c432333aa1095a80db8f21c13b96f
JC Lee <jcleefw@gmail.com>
2016-07-07 11:29:26 +1000

    Limit file extension on upload

--- app/js/components/uploader.js
+++ app/js/components/uploader.js
@@ -1,19 +1,19 @@
 class Uploader {
   constructor(imageList) {
     this.imageList = imageList
+    this.fileElem = document.querySelector('#file-upload')
     document.addEventListener('click', this.onUploadSubmit.bind(this))
   }
 
   onUploadSubmit(e) {
     if (e.target.id !== 'submit-upload') return false
 
-    const date = new Date()
-    const pathname = `${S3_BUCKET_PREFIX}/${date.getFullYear()}/${date.getFullMonth()}/`
-    const fileUploader = document.querySelector('#file-upload')
-
     const promises = [];
-    for (let i = 0; i < fileUploader.files.length; i++) {
-      promises.push(App.s3Service.upload(fileUploader.files[i], pathname))
+    for (let i = 0; i < this.fileElem.files.length; i++) {
+      const file = this.fileElem.files[i]
+      let key = this.buildS3Key(file)
+
+      promises.push(App.s3Service.upload(file, key))
     }
 
     Promise.all(promises)
@@ -23,4 +23,19 @@ class Uploader {
   onUploadSuccess() {
     this.imageList.fetchImages()
   }
+
+  buildS3Key(file) {
+    let bucket = ""
+    if(file.type.contains(ACCEPTED_IMAGES))
+      bucket = S3_IMAGES_PREFIX
+    else if(file.type.contains(ACCEPTED_PDF))
+      bucket = S3_PDF_PREFIX
+
+    const date = new Date()
+    return `${bucket}/${date.getFullYear()}/${date.getFullMonth()}/${file.name}`
+  }
+
+
+
+
 }
