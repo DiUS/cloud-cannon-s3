44b1134fc01f48bd7c1beb922372d1e1c682fd55
parlux <paulcarnibella@gmail.com>
2016-07-13 11:34:59 +1000

    Added script with global var to monitor the current CKEDITOR

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,7 @@ const App = { s3Service: new S3Service() }
 App.s3Service.configure()
   .then(_ => {
     new Container()
+    const ckMonitor = new CkMonitor()
     const assetDrawer = new AssetDrawer()
     const assetList = new AssetList()
     const search = new Search(assetList)
